faa1ea28823ae9e7cc681521dc7ecec36f604285
carolinewh <cwhitman369@gmail.com>
2017-09-28 15:28:44 -0400

    Updated Menu Options
    Edited options in hamburger menu

--- SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/NavigationDrawerActivity.java
+++ SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/NavigationDrawerActivity.java
@@ -80,17 +80,17 @@ public class NavigationDrawerActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.all_lists) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.lists_in_progress) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.completed_lists) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.create_list) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nearby_sites) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.top_lists) {
 
         }
 
