cd09a2dbce44f9b0db9e6599338c109189c280db
nrs007 <nrs007@bucknell.edu>
2017-10-14 17:10:17 -0400

    some more databasey stuff

--- SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/NewListActivity.java
+++ SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/NewListActivity.java
@@ -2,13 +2,38 @@ package edu.bucknell.seniordesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class NewListActivity extends AppCompatActivity {
+
+    private Button mNewListButton;
+    private FirebaseDatabase db;
+    public static DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_list);
+
+        //mRef = new FirebaseDatabase("https://natparksdb.firebaseio.com/");
+        db = FirebaseDatabase.getInstance();
+        DatabaseReference mRef = db.getReference("https://natparksdb.firebaseio.com/");
+
+
+        mNewListButton = (Button) findViewById(R.id.newListButton);
+
+        mNewListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View viw) {
+                DatabaseReference mRefChild = mRef.child("Name");
+
+            }
+        });
+
     }
 }
