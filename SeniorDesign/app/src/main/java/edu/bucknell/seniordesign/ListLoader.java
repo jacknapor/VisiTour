6ae2f97426fdafc2a84e558b1e02c76f6177dc78
jrn011 <jrn011@bucknell.edu>
2017-10-15 12:40:30 -0400

    added onitemclicklistener
    added onitemclicklistener

--- SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/ListLoader.java
+++ SeniorDesign/app/src/main/java/edu/bucknell/seniordesign/ListLoader.java
@@ -21,18 +21,18 @@ import static android.R.layout.simple_list_item_1;
  * Created by Jack on 10/14/2017.
  */
 
-public class ListLoader extends AppCompatActivity{
+public class ListLoader extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_list);
         ListView v = (ListView) findViewById(R.id.list);
-        List n= (List) this.getIntent().getSerializableExtra("list");
-        Location x[]=n.getLocationArray();
-        String names[]= new String[x.length];
-        for (int i=0;i<= x.length-1; i++){
-            names[i]=x[i].getLocationName();
+        List n = (List) this.getIntent().getSerializableExtra("list");
+        Location x[] = n.getLocationArray();
+        String names[] = new String[x.length];
+        for (int i = 0; i <= x.length - 1; i++) {
+            names[i] = x[i].getLocationName();
         }
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
@@ -40,6 +40,15 @@ public class ListLoader extends AppCompatActivity{
         v.setAdapter(adapter);
         final Activity activity = this;
         activity.setTitle(n.getListName());
+        v.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                int itemPosition = position;
+            }
+        });
+
 
     }
 }
